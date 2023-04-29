2f9cf6cfea14a6dce54c9664036056ede616c94b
John <benjamin.dada@fcmb.com>
2023-04-29 15:39:36 +0100

    Doubly linkedlist

---
+++ StackQueueDequeue/Deque.cs
@@ -0,0 +1,24 @@
+﻿using StackQueueDeque;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackQueueDequeue
+{
+    public class Deque<T> : IEnumerable<T>
+    {
+        readonly DoublyLinkedList<T> store = new();
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
