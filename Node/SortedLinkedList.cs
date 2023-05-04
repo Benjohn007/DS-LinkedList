3fb22bcd80f7bfca77a899677241e97231a82fad
John <benjamin.dada@fcmb.com>
2023-05-04 23:52:52 +0100

    sorted Linkedlist

---
+++ Node/SortedLinkedList.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    public class SortedLinkedListNode<T>
+    {
+        public T data { get; set; }
+        public SortedLinkedListNode<T> next { get; set; }
+        public SortedLinkedListNode<T> prev { get; set; }
+        public SortedLinkedListNode(T value) 
+        {
+            data = value;
+        }
+    }
+    public class SortedLinkedList<T>
+    {
+        public SortedLinkedListNode<T> Head;
+        public SortedLinkedListNode<T> Tail;
+
+        public int Count { get; private set; }
+
+        public void Add(T value)
+        {
+            SortedLinkedListNode<T> node = new(value);
+            if (Head == null)
+            {
+                Head = node;
+                Tail = Head;
+            }
+            else if(Head.data.Equals(value).CompareTo(value) >= 0) 
+            {
+                SortedLinkedListNode<T> newHead = new(value);
+                newHead.next = Head;
+                Head.prev = newHead;
+                Head = newHead;
+            }
+            else if(Tail.data.Equals(value).CompareTo(value) < 0)
+            {
+                SortedLinkedListNode<T> newTail = new(value);
+                newTail.prev = Tail;
+                Tail.next = newTail;
+                Tail = newTail;
+            }
+            else
+            {
+                SortedLinkedListNode<T> insertBefore = Head;
+                while(insertBefore.data.Equals(value).CompareTo(value) <0) 
+                {
+                    insertBefore = insertBefore.next;
+                }
+
+                SortedLinkedListNode<T> toInsert = new(value);
+                toInsert.next = insertBefore;
+                toInsert.prev = insertBefore.prev;
+                insertBefore.prev.next = toInsert;
+                insertBefore.prev = toInsert;
+            }
+            Count++;
+        }
+    }
+}
--- StackQueueDequeue/DoublyLinkedList.cs
+++ StackQueueDequeue/DoublyLinkedList.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,152 @@ using System.Threading.Tasks;
 
 namespace StackQueueDeque
 {
-    public class DoublyLinkedList<T>
+    public class DoublyLinkedList<T> : ICollection
     {
+        public DoublyLinkedListNode<T>? Head
+        {
+            get;
+            private set;
+        }
+        public DoublyLinkedListNode<T>? Tail
+        {
+            get;
+            private set;
+        }
+
+        int ICollection.Count => throw new NotImplementedException();
+
+        public bool IsSynchronized => throw new NotImplementedException();
+
+        public object SyncRoot => throw new NotImplementedException();
+
+        public int Count;
+
+        public void AddHead(T value)
+        {
+            DoublyLinkedListNode<T> adding = new 
+                DoublyLinkedListNode<T>(value);
+
+            if(Head != null)
+            {
+                Head.Previous = adding;
+            }
+            Head = adding;
+            if(Tail == null)
+            {
+                Tail = Head;
+            }
+            Count++;
+        }
+        public void AddTail(T value)
+        {
+            DoublyLinkedListNode<T> adding = new
+               DoublyLinkedListNode<T>(value);
+
+            if(Tail == null)
+            {
+                AddHead(value);
+            }
+            else
+            {
+                Tail.Next = adding;
+                Tail = adding;
+
+                Count++;
+            }
+        }
+
+        private DoublyLinkedListNode<T> Find(T value)
+        {
+            DoublyLinkedListNode<T>? current = Head;
+
+            while(current != null)
+            {
+                if(current.Value.Equals(value)) return current;
+
+                current = current.Next;
+            }
+
+            return null;
+        }
+
+        public bool Contains(T value)
+        {
+            return Find(value) != null;
+        }
+        
+        public bool Remove(T item)
+        {
+            DoublyLinkedListNode<T> found = Find(item);
+            if(found == null)
+            {
+                return false;
+            }
+
+            DoublyLinkedListNode<T> previous = found.Previous;
+            DoublyLinkedListNode<T> next = found.Next;
+
+            if(previous == null)
+            {
+                Head = next;
+                if(Head != null)
+                {
+                    Head.Previous = null;
+                }
+                else
+                {
+                    previous.Next = next;
+                }
+
+                if(next == null)
+                {
+                    Tail = previous;
+                    if(Tail != null)
+                    {
+                        //removing the tail
+                        Tail.Next = null;
+                    }
+                    else
+                    {
+                        next.Previous = previous;
+                    }
+                }
+            }
+            Count--;
+
+            return true;
+        }
+
+        public System.Collections.Generic.IEnumerator<T> GetEnumerator()
+        {
+            DoublyLinkedListNode<T> current = Head;
+            while(current != null)
+            {
+                yield return current.Value; 
+                current = current.Next;
+            }
+        }
+
+        
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public System.Collections.Generic.IEnumerator<T> GetReverseEnumerator()
+        {
+            DoublyLinkedListNode<T> current = Tail;
+            while (current != null)
+            {
+                yield return current.Value;
+                current = current.Previous;
+            }
+        }
+
+        public void CopyTo(Array array, int index)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
